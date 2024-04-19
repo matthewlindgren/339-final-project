90bda832c18e0a5203962e22b843cb8cf4777f90
matthewlindgren <lindmatt@umich.edu>
2024-04-19 11:06:28 -0400

    Added mixitup

--- js/script.js
+++ js/script.js
@@ -6,4 +6,24 @@ document.querySelector('.search').addEventListener('click', function() {
     document.querySelector('#search-field').classList.toggle('shrink-search');
     document.querySelector('.search').classList.toggle('spin');
     document.querySelector('.search').classList.toggle('spin-reverse');
-})
+});
+
+collapsed = false;
+document.querySelector('.sidebar-button').addEventListener('click', function() {
+    document.querySelectorAll('nav a')[1].classList.toggle('hidden');
+    document.querySelectorAll('nav a')[3].classList.toggle('hidden');
+    document.querySelectorAll('nav a')[5].classList.toggle('hidden');
+    if (!collapsed) {
+        document.querySelector('header').style.width = "50px";
+        this.style.left = "60px";
+        this.style.rotate = "0deg";
+        document.querySelector('main').style.marginLeft = "80px";
+        collapsed = true;
+    } else {
+        document.querySelector('header').style.width = "120px";
+        this.style.left = "128px";
+        this.style.rotate = "180deg";
+        document.querySelector('main').style.marginLeft = "150px";
+        collapsed = false;
+    }
+});
